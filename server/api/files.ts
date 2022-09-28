4668c76c568c0c895e683b0a9284356a7b610fd4
Andrew G. Knackstedt <andrew.knackstedt@dynatrace.com>
2022-09-28 01:49:37 -0400

    Update backend to handle files as arrays

--- server/api/files.ts
+++ server/api/files.ts
@@ -2,7 +2,7 @@ import * as express from "express";
 import { route, getFilesInFolder } from '../util';
 import fs from 'fs';
 import { readFile, stat, access } from "fs-extra";
-import { isText, isBinary, getEncoding } from 'istextorbinary';
+import { isText } from 'istextorbinary';
 import mime from "mime-types";
 import AdmZip from "adm-zip";
 import tar from "tar";
@@ -96,6 +96,7 @@ router.use('/tar', route(async (req, res, next) => {
 
 router.use('/download', route(async (req, res, next) => {
     const { dir, file } = req.query as any;
+    if (!dir || !file) return next(400);
 
     try {
         let stats = await stat(dir + file);
@@ -131,36 +132,37 @@ router.use('/download', route(async (req, res, next) => {
 
 router.use('/file', route(async (req, res, next) => {
     // TODO: save file
-    const { dir, file } = req.body;
+    const file = req.body;
+    const { only } = req.query;
+
+    if (!file) return next(400);
 
     const files = Array.isArray(file) ? file : [file];
 
     Promise.all(files.map(file => new Promise(async (resolve, reject) => {
-        const path = dir + file;
 
         const ct = mime.lookup(file) || "application/octet-stream";
 
         res.setHeader("content-type", ct);
-        let stats = await stat(path);
+        let stats = await stat(file);
 
-        if (isText(path)) {
-            let data = await readFile(path, { encoding: "utf-8" });
+        if (isText(file)) {
 
             resolve({
                 name: file,
                 meta: stats,
                 type: "text",
-                content: data
+                content: only != "stat" && await readFile(file, { encoding: "utf-8" })
             })
         }
         else if (stats.size < 2 * 1024 * 1024) {
-            let buf = await readFile(path);
+            let buf = await readFile(file);
             if (isText(null, buf)) {
                 resolve({
                     name: file,
                     meta: stats,
                     type: "text",
-                    content: buf.toString(),
+                    content: only != "stat" && buf.toString()
                 });
             }
             else {
@@ -185,12 +187,14 @@ router.use('/file', route(async (req, res, next) => {
 
 router.use('/', route(async (req, res, next) => {
     const { path, showHidden } = req.body;
+    
+    if (!path) return next(400);
 
     // If the file doesn't exist
     let hasAccess = await access(path, fs.constants.F_OK | fs.constants.W_OK | fs.constants.R_OK)
         .then(r => true)
         .catch(e => {
-            res.send(e);
+            next(e);
         })
     
     if (!hasAccess) return;
