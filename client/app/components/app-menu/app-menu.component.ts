69396495be3fb716cd0d1524562c9d85c052f681
Andrew G. Knackstedt <andrew.knackstedt@dynatrace.com>
2022-10-08 08:43:28 -0400

    Update app menu

--- client/app/components/app-menu/app-menu.component.ts
+++ client/app/components/app-menu/app-menu.component.ts
@@ -3,7 +3,13 @@ import { Apps } from 'client/app/applications';
 import { WindowManagerService } from '../../services/window-manager.service';
 import { MatDialogRef } from '@angular/material/dialog';
 
-
+const charMap = {
+    "[": "\\[",
+    "]": "\\]",
+    "^": "\\^",
+    "-": "\\-",
+    "\\": "\\\\"
+}
 
 @Component({
     selector: 'app-app-menu',
@@ -11,8 +17,44 @@ import { MatDialogRef } from '@angular/material/dialog';
     styleUrls: ['./app-menu.component.scss']
 })
 export class AppMenuComponent {
+    search = "";
 
     apps = Apps;
+
+    _filterApps() {
+        const regex = this.search.split('')
+            .map(c => charMap[c] || c)
+            .map((c, i) => `([${c}])`)
+            .join(".*?");
+        const rx = new RegExp(regex, 'i');
+
+        return this.apps.filter(a => rx.test(a.title) || rx.test(a.description));
+    }
+    // renderAppName(text) {
+    //     if (this.search.length == 0) return text;
+
+    //     const regex = this.search.split('')
+    //         .map(c => charMap[c] || c)
+    //         .map((c, i) => `(?<_${i}>[${c}])`)
+    //         .join(".*?");
+
+    //     const rx = new RegExp(regex, 'i');
+    //     text.replace(rx, (...args) => {
+    //         // const groups = args.pop();
+    //         // Object.keys(groups).forEach()
+    //         // debugger;
+    //     })
+    // }
+
+    folders = [
+        {
+            icon: "",
+            title: "",
+            apps: [
+                "..."
+            ]
+        }
+    ]
 
     constructor(public windowManager: WindowManagerService, public dialogRef: MatDialogRef<any>) { }
 }
