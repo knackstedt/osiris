89302b5f217ae72abc36147f3e05b93a2abf01ef
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-03-07 20:44:58 -0500

    major ctx rework

--- client/app/directives/context-menu.directive.ts
+++ client/app/directives/context-menu.directive.ts
@@ -1,12 +1,75 @@
-import { Directive, Input, ViewContainerRef, ElementRef, HostListener, TemplateRef } from '@angular/core';
-import { ContextMenuItem, ContextMenuComponent } from '../components/context-menu/context-menu.component';
+import { Directive, Input, HostListener, TemplateRef } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { KeyCommand } from '../services/keyboard.service';
+import { calcMenuItemBounds, ContextMenuComponent } from 'client/app/components/@framework/context-menu/context-menu.component';
 
-// length in ms for a long touch to activate
-const longTouchTime = 600;
+type BaseCtx<T = any> = {
+    /**
+     * Label for the menu-item
+     */
+    label: string,
+
+    /**
+     * Callback method that is called when a user activates
+     * a context-menu item.
+     * Use the `contextMenuData` decorator for passing data.
+     */
+    action?: (evt: MouseEvent, data: T) => any,
+
+    /**
+     * Callback method that is called upon a context menu activation
+     * that when returning false, will disable menu items.
+     */
+    canActivate?: (data: T) => boolean,
+
+    /**
+     * Set whether the menu item appear disabled.
+     */
+    disabled?: boolean,
+
+    /**
+     * If a shortcut is set, the text-label.
+     */
+    shortcutLabel?: string,
+
+    /**
+     * Keyboard shortcut to activate this item.
+     */
+    shortcut?: KeyCommand,
+
+    /**
+     * Path to an icon to render on the left side of the item.
+     */
+    icon?: string,
+
+    /**
+     * Optional child menu
+     */
+    children?: ContextMenuItem[],
+
+    /**
+     * Instead of an array of children, render a template
+     */
+    childTemplate?: TemplateRef<any>;
+    /**
+     * Width of child component
+     */
+    childWidth?: number,
+    /**
+     * Height of child component
+     */
+    childHeight?: number,
+
+    seperator?: boolean;
+};
+
+export type ContextMenuItem<T = any> =
+    BaseCtx<T> |
+    "seperator";
 
 @Directive({
-    selector: '[contextMenu]'
+    selector: '[contextMenu]',
+    standalone: true
 })
 export class ContextMenuDirective {
 
@@ -21,106 +84,58 @@ export class ContextMenuDirective {
     @Input("contextMenuItems") menuItems: ContextMenuItem[];
 
     constructor(
-        private element: ElementRef,
-        private viewContainerRef: ViewContainerRef,
         private dialog: MatDialog
-    ) { 
-        
-    }
+    ) { }
 
-    private calcHeight() {
-        return this.menuItems
-            .map(i => typeof i == "string" ? 2 : 24)
-            .reduce((a, b) => a+b, 0);
-    }
-
-    private calcWidth() {
-
-        const items = this.menuItems
-            .filter(i => i != "seperator");
-
-        const lName =  (items as any)
-            .sort((a, b) => a.label.length - b.label.length)
-            .pop().label;
-        const lShort = (items as any)
-            .sort((a, b) => a.label.length - b.label.length)
-            .pop().label;
-
-        // Create dummy div that will calculate the width for us.
-        const div = document.createElement("div");
-        div.style["font-size"] = "14px";
-        div.style["position"] = "absolute";
-        div.style["opacity"] = "0";
-        div.style["pointer-events"] = "none";
-        div.style["left"] = "-1000vw";
-
-        div.innerText = lName + lShort;
-
-        document.body.appendChild(div);
-
-        // Get width
-        const w = div.getBoundingClientRect().width;
-
-        // Clear element out of DOM
-        div.remove();
-
-        return w;
-    }
-
+    // Needs to be public so we can manually open the dialog
     @HostListener('contextmenu', ['$event'])
-    private onContextMenu(evt: MouseEvent) {
+    public onContextMenu(evt: MouseEvent) {
         evt.preventDefault();
 
-        const h = this.calcHeight();
-        const w = this.calcWidth();
+        const { width, height } = calcMenuItemBounds(this.menuItems);
 
         const cords = {
             top: null,
             left: null,
             bottom: null,
             right: null
-        }
+        };
 
-        if (evt.clientY + h > window.innerHeight)
+        if (evt.clientY + height > window.innerHeight)
             cords.bottom = (window.innerHeight - evt.clientY) + "px";
-        if (evt.clientX + w > window.innerWidth)
+        if (evt.clientX + width > window.innerWidth)
             cords.right = (window.innerWidth - evt.clientX) + "px";
 
         if (!cords.bottom) cords.top = evt.clientY + "px";
         if (!cords.right) cords.left = evt.clientX + "px";
 
-        const dialogRef = this.dialog.open(ContextMenuComponent, {
+        let items = [];
+        this.menuItems.forEach((item: ContextMenuItem) => {
+            items.push(typeof item == "string" ? item : {
+                ...item,
+                active: typeof item.canActivate == "function" ? item.canActivate(this.data) : true
+            });
+        });
+
+        // Create the context menu
+        this.dialog.open(ContextMenuComponent, {
             data: {
                 data: this.data,
-                items: this.menuItems
+                items: items
             },
             panelClass: "context-menu",
             position: cords,
             backdropClass: "context-menu-backdrop"
         });
-
-        dialogRef.afterClosed().subscribe(result => {
-            console.log('The dialog was closed');
-        });
     }
-
-    // private _timer;
-    // @HostListener('touchstart', ['$event']) 
-    // private onTouchStart(evt) {
-    //     this._timer = setTimeout(() => {
-    //         if (this._timer) {
-    //             clearTimeout(this._timer)
-    //             this._timer = null;
-    //         }
-
-    //         this.onContextMenu(evt);
-    //     }, longTouchTime);
-    // } 
-    // @HostListener('touchend', ['$event'])
-    // private onTouchEnd(evt) {
-    //     if (this._timer) {
-    //         clearTimeout(this._timer)
-    //         this._timer = null;
-    //     }
-    // }
 }
+
+// Helper to open the context menu without using the directive.
+export const openContextMenu = (dialog: MatDialog, menuItems: ContextMenuItem[], data: any, evt: PointerEvent) => {
+    const ctx = new ContextMenuDirective(dialog);
+    ctx.data = data;
+    ctx.menuItems = menuItems;
+    ctx.onContextMenu(evt);
+
+    // TODO: is this disposed properly?
+};
