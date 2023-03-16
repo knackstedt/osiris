ff6ca2548cb030396c14a55280862bce4ed629af
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-03-16 08:25:42 -0400

    Improve zip directory traversal

--- server/api/files.ts
+++ server/api/files.ts
@@ -216,35 +216,65 @@ router.use('/checksum/:type', route(async (req, res, next) => {
 }));
 
 // TODO: support reading subfolders!
-const readZip = (absPath) => {
-    let zip = new AdmZip(absPath);
-    let zipEntries = zip.getEntries();
+const readZip = (absPath: string, subpath = '') => {
+    absPath = absPath.replace(/\/{2,}/g, '/'); // collapse extra slashes
+    subpath = subpath.replace(/\/{2,}/g, '/');
+    const zip = new AdmZip(absPath);
+    const zipEntries = zip.getEntries();
 
-    let out = {
+    const zipName = absPath.split('/').pop().replace(/\.zip$/, '');
+
+    const out = {
         dirs: [],
-        files: [],
+        files: []
     };
 
-    zipEntries.forEach(e => {
-        let comment = zip.getZipEntryComment(e);
+    const localPath = zipName + '/' + subpath;
+    const subpathMatcher = new RegExp('[' + subpath.split('').join('][') + ']\/?');
 
-        if (e.isDirectory) {
+    function filterLocalPath(entry: AdmZip.IZipEntry) {
+        // Omit anything that's in a different directory altogether
+        if (!entry.entryName.startsWith(subpath)) return false;
+
+        // Skip if the entry is the item selected from the subpath
+        if (entry.entryName.replace(subpath, '').length <= 1) return false;
+
+        // Check if the entry is the root file
+        // if (entry.isDirectory && entry.entryName != (localPath + '/')) return false;
+
+        // Check if the file is nested deeper than we're looking
+        const local = entry.entryName.replace(subpathMatcher, '');
+        if (!local || local.split('/').filter(e => e).length > 1) return false;
+
+        return true
+    }
+
+    zipEntries
+        .filter(e => e.isDirectory)
+        .filter(e => filterLocalPath(e))
+        .forEach(e => {
+            let path = absPath + "#/" + e.entryName.split('/').slice(0, -2).join('/') + '/';
+            path = path.replace(/\/{2,}/g, '/');
+
             out.dirs.push({
-                contents: {
-                    dirs: [],
-                    files: []
-                },
-                path: absPath + "#/" + e.entryName.split('/').slice(0, -2).join('/'),
+                path: path,
                 name: e.entryName.split('/').slice(-2, -1)[0],
                 kind: "directory",
-                comment
+                comment: e.comment,
+                entry: e.entryName
             })
-        }
-        else {
-            const parts = e.entryName.split('/');
-            const entry = {
+        });
+
+    zipEntries
+        .filter(e => !e.isDirectory)
+        .filter(e => filterLocalPath(e))
+        .forEach(e => {
+            let path = absPath + "#/" + e.entryName.split('/').slice(0, -1).join('/');
+            path = path.replace(/\/{2,}/g, '/');
+
+            out.files.push({
                 kind: "file",
-                path: absPath + "#/",
+                path: path,
                 name: e.name,
                 ext: e.name.split('.').pop(),
                 stats: {
@@ -253,44 +283,25 @@ const readZip = (absPath) => {
                     mtimeMs: e.header.time.getTime(),
                     atimeMs: e.header.time.getTime(),
                     ctimeMs: e.header.time.getTime()
-                },
-                comment
-            };
-            if (parts.length > 1) {
-                let _ptr = out;
-
-                for (let i = 0; i < parts.length; i++) {
-                    let currentPath = absPath + "#/" + parts.slice(0, i).join('/');
-
-                    let next = _ptr.dirs.find(d => d.path == currentPath);
-                    if (!next) {
-                        _ptr.dirs.push(next = {
-                            contents: {
-                                dirs: [],
-                                files: []
-                            },
-                            kind: "directory",
-                            path: currentPath,
-                            name: parts.slice(i-1, 1)[0]
-                        });
-                    }
-                    _ptr = next.contents;
                 }
-
-                entry.path = absPath + "#/" + parts.slice(0, -1).join('/') + "/",
-                _ptr.files.push(entry);
-            }
-            else {
-                out.files.push(entry);
-            }
-        }
-    })
+            });
+        });
 
     return out;
 };
 
 router.use('/', route(async (req, res, next) => {
-    const { path, showHidden } = req.body;
+    let { path, showHidden } = req.body;
+
+    // A path that filters the contents returned for an archive
+    let archiveSubpath: string;
+
+    // TODO: make this work better
+    if (path.includes(".zip#/")) {
+        let [outerpath, innerpath] = path.split("#/");
+        path = outerpath;
+        archiveSubpath = innerpath;
+    }
 
     if (!path) return next(400);
 
@@ -314,7 +325,7 @@ router.use('/', route(async (req, res, next) => {
     else if (stats.isFile()) {
         const ext = (path.split('.').pop());
         if (ext == "zip") {
-            res.send(readZip(path));
+            res.send(readZip(path, archiveSubpath));
         }
         else {
             next({
