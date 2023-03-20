2625ec4f5b02fd98cb8fe5036010e6a5340d6df2
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-03-19 21:53:08 -0400

    Add workspace background

--- client/app/apps/client-settings/client-settings.component.ts
+++ client/app/apps/client-settings/client-settings.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ConfigurationService } from '../../services/configuration.service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { WindowTemplateComponent } from '../../components/window-template/window-template.component';
+import { GtkTextboxComponent } from '../gtk-factory/@components/textbox/textbox.component';
 
 @Component({
     selector: 'app-client-settings',
@@ -13,11 +15,14 @@ import { MatIconModule } from '@angular/material/icon';
         CommonModule,
         FormsModule,
         MatButtonModule,
-        MatIconModule
+        MatIconModule,
+        WindowTemplateComponent,
+        GtkTextboxComponent
     ],
     standalone: true
 })
 export class ClientSettingsComponent implements OnInit {
+    @Input() window;
 
     constructor(
         public config: ConfigurationService
