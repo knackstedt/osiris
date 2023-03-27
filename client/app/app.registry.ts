324c22a2d82881ba94ac1f8611d00351e460ea07
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-03-26 23:40:02 -0400

    amend missing loader

--- client/app/app.registry.ts
+++ client/app/app.registry.ts
@@ -14,7 +14,6 @@ export const RegisteredApplications: ComponentRegistration[] = [
     { id: 'client-settings', group: "app", load: () => import('client/app/apps/client-settings/client-settings.component'),   iconType: "mat-icon", iconSrc: "settings", label: "Settings", order: 0 },
     { id: 'media-player',    group: "app", load: () => import('client/app/apps/video-player/video-player.component'),         iconType: "mat-icon", iconSrc: "smart_display", label: "Videos", order: 0 },
     { id: 'music-library',   group: "app", load: () => import('client/app/apps/music-library/music-library.component'),         iconType: "mat-icon", iconSrc: "library_music", label: "Videos", order: 0 },
-    { id: 'music-mini',      group: "app", load: () => import('client/app/apps/music-library/mini-player/mini-player.component'), iconType: "mat-icon", iconSrc: "album", label: "Videos", order: 0 },
     { id: 'text-editor',     group: "app", load: () => import('client/app/apps/code-editor/code-editor.component'),           iconType: "mat-icon", iconSrc: "code", label: "Text Editor", order: 0 },
     { id: 'log-viewer',      group: "app", load: () => import('client/app/apps/code-editor/log-reader/log-reader.component'), iconType: "mat-icon", iconSrc: "article", label: "Log Viewer", order: 0 },
     { id: 'welcome',         group: "app", load: () => import('client/app/apps/introduction/introduction.component'),         iconType: "mat-icon", iconSrc: "help", label: "Welcome", order: 0 },
