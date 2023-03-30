b77304425beb90cf09843fce9952b059c233c139
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-03-29 23:49:51 -0400

    cleanup

--- client/app/components/tabulator/tabulator.component.ts
+++ client/app/components/tabulator/tabulator.component.ts
@@ -20,7 +20,6 @@ export class TabulatorComponent<T = any> {
 
     private _dataSource = [];
     @Input() set dataSource(data: Object[]) {
-        console.log("SET DATAS", !!this.table, this.table, data)
         this._dataSource = data;
 
         this.table?.setData(this.dataSource);
@@ -48,29 +47,16 @@ export class TabulatorComponent<T = any> {
     ngAfterViewInit() {
         const table = this.table = new Tabulator(this.tableRef.nativeElement, {
             data: this._dataSource,
-            // reactiveData: true, //enable data reactivity
+            reactiveData: true,
             columns: this._columns,
             layout: 'fitDataFill',
             height: "100%",
-            maxHeight: window.innerHeight,
-            cellDblClick: console.log,
-            // cellClick: this.cellClick.next.bind(this),
-            // cellDblClick: this.cellDblClick.next.bind(this),
-            // rowClick: this.rowClick.next.bind(this),
-            // rowDblClick: this.rowDblClick.next.bind(this),
-            rowDblClick: console.log,
-            // rowSelected: this.rowSelected.next.bind(this),
+            maxHeight: window.innerHeight
         });
 
         table.on("rowClick", (e, row) => this.rowClick.next({ event: e, row, data: row.getData() }));
         table.on("rowContext", (e, row) => this.rowContext.next({ event: e, row, data: row.getData() }));
         table.on("rowDblClick", (e, row) => this.rowDblClick.next({ event: e, row, data: row.getData() }));
-        // table.on("rowClick", (e, row) => this.rowClick.next({ event: e, row, data: row.getData() }));
-        // table.on("rowClick", (e, row) => this.rowClick.next({ event: e, row, data: row.getData() }));
-        // table.on("rowClick", (e, row) => this.rowClick.next({ event: e, row, data: row.getData() }));
-        // table.on("rowClick", (e, row) => this.rowClick.next({ event: e, row, data: row.getData() }));
-        // table.on("rowClick", (e, row) => this.rowClick.next({ event: e, row, data: row.getData() }));
-        // table.on("rowClick", (e, row) => this.rowClick.next({ event: e, row, data: row.getData() }));
 
     }
 
