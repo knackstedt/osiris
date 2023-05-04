d182def07121bfbc310d20b1dc0eef59ba1a8a42
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-05-03 22:10:38 -0400

    fix shell script

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,4 @@
-if [[ -e node_modules/material-icon-theme/src/icons/generator/fileGenerator.ts ]]
+if [[ -f node_modules/material-icon-theme/src/icons/generator/fileGenerator.ts ]]
+then
     echo -e \"//@ts-nocheck\n$(cat node_modules/material-icon-theme/src/icons/generator/fileGenerator.ts)\" > node_modules/material-icon-theme/src/icons/generator/fileGenerator.ts
 fi
