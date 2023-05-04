ebfc6da7af7acb167823a58833230de30adc6981
Andrew G. Knackstedt <knackstedt@outlook.com>
2023-05-03 22:54:31 -0400

    Fix build error

--- postinstall.sh
+++ postinstall.sh
@@ -1,4 +1,4 @@
 if [[ -f node_modules/material-icon-theme/src/icons/generator/fileGenerator.ts ]]
 then
-    echo -e \"//@ts-nocheck\n$(cat node_modules/material-icon-theme/src/icons/generator/fileGenerator.ts)\" > node_modules/material-icon-theme/src/icons/generator/fileGenerator.ts
+    echo -e \"//@ts-nocheck\\n$(cat node_modules/material-icon-theme/src/icons/generator/fileGenerator.ts)\" > node_modules/material-icon-theme/src/icons/generator/fileGenerator.ts
 fi
